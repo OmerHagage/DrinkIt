ffa1e410715b2756e7151c62b54bc8b15196be22
omerhagage <omer.hagage@mail.huji.ac.il>
2020-09-06 14:24:37 +0300

    Initial Commit

---
+++ DrinkIt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrinkIt
+//
+//  Created by Omer Hagage on 06/09/2020.
+//  Copyright © 2020 Omer Hagage. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
